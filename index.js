f3004999a3b359edf397373b70e6a0a474f9a7ba
Arya Krishna <dr.aryakris@outlook.com>
2022-08-08 21:55:30 -0700

    starter files

---
+++ index.js
@@ -0,0 +1,13 @@
+// 1. Create two variables, firstCard and secondCard.
+// Set their values to a random number between 2-11 - - Creating Javascript Random Numbers with Math.random()
+
+
+// 2. Create a variable, sum, and set it to the sum of the two cards
+
+var firstCard;
+var secondCard;
+
+let firstCard = math.random()*11
+let secondCard = math.random() * 11
+
+var sum = firstCard + secondCard;
