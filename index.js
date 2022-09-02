f78ff556b062aff29daa783e366fceaa53df22d3
Arya Krishna <dr.aryakris@outlook.com>
2022-09-01 23:25:37 -0700

    math.random and math.floor function to pick out a randome card from 1-11 in the game

--- index.js
+++ index.js
@@ -1,68 +1,66 @@
-// 1. Create two variables, firstCard and secondCard.
-// Set their values to a random number between 2-11 - - Creating Javascript Random Numbers with Math.random()
+// 2. Use getRandomCard() to set the values of firstCard and secondCard
 
 
-// 2. Create a variable, sum, and set it to the sum of the two cards
-
-let firstCard = 10
-let secondCard = 4
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
-
-// 1. Store the message-el paragraph in a variable called messageEl
 let messageEl = document.getElementById("message-el")
-// 2. Store the sum paragraph in a variable called sumEl
 let sumEl = document.getElementById("sum-el")
-let cardEl = document.getElementById("cards-el")
+let cardsEl = document.getElementById("cards-el")
 
-function startGame(){
+// 1. Create a function, getRandomCard(), that always returns the number 5
+function getRandomCard() {
+    let randomNumer = Math.floor(Math.random() * 13) + 1
+    if (randomNumer > 10) {
+        return 10
+    } else if (randomNumer === 1) {
+        return 11
+    } else {
+        return randomNumer
+    }
+}
+
+
+
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    // Generate two random numbes
+    // Re-assign the cards and sum variables so that the game can start
+
     renderGame()
 }
 
 function renderGame() {
-    // 3. Render the sum on the page using this format -> "Sum: 14"
-    sumEl.textContent = "sum:" + sum
-
-     // Create a for loop that renders out all the cards instead of just two
-
-     for (let i =0; i<cards.length; i++){
-         cardsEl.textContent += cards[i] + " "
-     }
-    //render out first card and second card
-    cardEl.textContent = "cards:" 
-    for(let i=0;i<cards.length;i++){
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + " "
     }
 
-    
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
     }
-    // 2. Display the message in the messageEl using messageEl.textContent
-if (sum <= 20) {
-    message = "Do you want to draw a new card?"
-} else if (sum === 21) {
-    message = "You've got Blackjack!"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game!"
-    isAlive = false
+    messageEl.textContent = message
 }
-messageEl.textContent = message
 
 
-
-
-function newCard(){
-    console.log("Drawing a new card from the deck")
-
-    let card = 7
+function newCard() {
+    // 3. Use the getRandomCard() to set the value of card
+    let card = getRandomCard()
     sum += card
-
-    //push the card to the cards array
     cards.push(card)
     console.log(cards)
     renderGame()
 }
-
