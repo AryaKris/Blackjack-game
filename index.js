185df183094fbd4530bc7a92334e7b877d977cdc
Arya Krishna <dr.aryakris@outlook.com>
2022-09-05 13:41:24 -0700

    use an object to store player data

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// 2. Create the player object. Give it two keys, name and chips, and set their value
+
+let player = {
+    name: "Arya",
+    chips: "32"
+}
 // 2. Use getRandomCard() to set the values of firstCard and secondCard
 
 
@@ -10,11 +16,13 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
-let player = {
-    name: "Per",
-    chips: 145
-}
+// 3. Grab ahold of the player-el paragraph and store it in a variable called playerEl
+let playerEl = document.getElementById("player-el")
 
+
+// 4. Render the player's name and chips in playerEl
+
+playerEl.textContent = player.name + ": $" + player.chips
 
 let playerEl = document.getElementById("player-el")
 playerEl.textContent = player.name + ": $" + player.chips
